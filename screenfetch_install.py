fed62a2385db4657d6139decb48edd2707da5d14
jonaypython <ilovepython99@gmail.com>
2015-09-30 16:57:36 +0200

    Create screenfetch_install.py

---
+++ screenfetch_install.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import os
+import urllib.request
+import getpass
+
+print("""
+                              _____    _       _     
+ ___  ___ _ __ ___  ___ _ __ |  ___|__| |_ ___| |__  
+/ __|/ __| '__/ _ \/ _ \ '_ \| |_ / _ \ __/ __| '_ \ 
+\__ \ (__| | |  __/  __/ | | |  _|  __/ || (__| | | |
+|___/\___|_|  \___|\___|_| |_|_|  \___|\__\___|_| |_|
+                                                     
+By : Jonay ,Administrador de  : https://www.facebook.com/InSeguridadInformaticaSt
+Nassima , Administradora de www.sanvendida.com , de Inseguridadinformática y editora de cyberpunk, facebook.com/Cyberpunks.Hackers.2015
+diego , Administrador de Inseguridadinformática y estudiante de marketing""")
+
+
+
+if getpass.getuser() == "root":
+    directorio = os.chdir("/usr/bin/")  
+    descarga = urllib.request.urlretrieve("https://raw.github.com/KittyKatt/screenFetch/master/screenfetch-dev " , "screenfetch")
+    permisos = ("sudo chmod +x screenfetch")
+    print ("Ahora que se a instalado correctamente,para que se ejecute siempre desde la terminal,lea el archivo terminal.txt")
+else:
+    print("Debes de ejecutar el script con sudo : sudo screenfetch_install.py ")
+
+
